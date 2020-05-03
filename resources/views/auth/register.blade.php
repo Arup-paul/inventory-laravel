946ab7c603a56c33da05b01d390f5147fe640dfe
Arup <arupctg@gmail.com>
2020-05-03 06:43:01 -0800

    register page customize

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+{{--
+<div class="wrapper-page">
+    <div class="panel panel-color panel-primary panel-pages">
+        <div class="panel-heading bg-img">
+
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
@@ -72,6 +74,90 @@
                 </div>
             </div>
         </div>
+    </div>
+</div> --}}
+
+<div class="wrapper-page">
+    <div class="panel panel-color panel-primary panel-pages">
+        <div class="panel-heading bg-img">
+            <div class="bg-overlay"></div>
+           <h3 class="text-center m-t-10 text-white"> Create a new Account </h3>
+        </div>
+
+
+        <div class="panel-body">
+         <form class="form-horizontal m-t-20" method="POST" action="{{ route('register') }}">
+            @csrf
+            <div class="form-group">
+                <div class="col-xs-12">
+                    <input id="name" type="text" class="form-control input-lg @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="username"utofocus>
+
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group ">
+                <div class="col-xs-12">
+                    <input id="email" type="email" class="form-control input-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="email" required autocomplete="email">
+
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-xs-12">
+                    <input id="password" type="password" class="form-control input-lg @error('password') is-invalid @enderror" name="password" required placeholder="password" autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-xs-12">
+                    <input id="password-confirm" type="password" placeholder="confirm password" class="form-control input-lg" name="password_confirmation" required autocomplete="new-password">
+                </div>
+            </div>
+
+            <div class="form-group ">
+                <div class="col-xs-12">
+                    <div class="checkbox checkbox-primary">
+                        <input id="checkbox-signup" type="checkbox" checked="">
+                        <label for="checkbox-signup">
+                            I accept <a href="#">Terms and Conditions</a>
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="form-group text-center m-t-40">
+                <div class="col-xs-12">
+                    <button type="submit" class="btn btn-primary waves-effect waves-light btn-lg w-lg">
+                        {{ __('Register') }}
+                    </button>
+                </div>
+            </div>
+
+            <div class="form-group m-t-30">
+                <div class="col-sm-12 text-center">
+                    <a href="{{ route('login') }}">Already have account?</a>
+                </div>
+            </div>
+        </form>
+        </div>
+
     </div>
 </div>
 @endsection
