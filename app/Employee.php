982e2ac7a0c729486a041710055f77abb1e9e2a3
Arup <arupctg@gmail.com>
2020-05-03 07:20:45 -0800

    employee controller and model page

---
+++ app/Employee.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    //
+    protected $fillable = [
+        'name', 'email', 'phone','address','experience','photo','salary','vacation','city'
+    ];
+}
---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+}
