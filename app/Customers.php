45907b880d64bbfa2570a5b96f9788fa3c9be654
Arup <arupctg@gmail.com>
2020-05-05 06:48:54 -0800

    customer model

--- app/Customers.php
+++ app/Customers.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Customers extends Model
 {
     //
+    protected $fillable = [
+        'name', 'email', 'phone', 'address', 'shop_name', 'photo', 'account_holder', 'account_number', 'bank_name','bank_branch','city'
+    ];
 }
